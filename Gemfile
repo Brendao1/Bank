c9526858cf7f1d33a9b63d089fe527b622409b39
“Brenda” <odonnell.brenda@gmail.com>
2019-04-11 12:48:41 +0100

    Print_statement_spec moved from bank_spec to statement_spec

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'coveralls', require: false
+
 # gem "rails"
 gem 'terminal-table', '~> 1.8'
 gem 'travis', '~> 1.8', '>= 1.8.8'
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe "A Statement" do
         expect(s.transactions.length).to eq 0
     end
 
+    it "renders the table with the transactions" do
+        expected_table = <<-EOF
+        +------------+--------+-------+---------+
+        | date       | credit | debit | balance |
+        +------------+--------+-------+---------+
+        | 16/02/2019 |        | 5     | 45      |
+        | 15/02/2019 | 50     |       | 50      |
+        +------------+--------+-------+---------+
+   EOF
+        a = Account.new
+        a.deposit(50, "15/02/2019")
+        a.store_transaction
+        a.withdrawal(5, "16/02/2019")
+        a.store_transaction
+        expect(STDOUT).to receive(:puts)
+        expect{a.print_statement}.to output(expected_table).to_stdout
+    end
+
+
 end
